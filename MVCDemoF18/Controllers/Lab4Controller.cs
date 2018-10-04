430dfeaa88f02ee103ccb61f950a058e2c815540
olanmj <olanm@stockton.edu>
2018-10-04 10:22:04 -0400

    Add date time and wafflegenerator

---
+++ MVCDemoF18/Controllers/Lab4Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaffleGenerator;
+
+namespace MVCDemoF18.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime date = DateTime.Now;
+            return View(date);
+        }
+
+        public IActionResult Waffler()
+        {
+            var waffle = WaffleEngine.Html(2, true, false);
+            return View(waffle);
+        }
+    }
+}
