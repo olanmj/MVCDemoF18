1260b18c0bf1480d6b82cdcdc5ee418fda3f31d1
olanmj <olanm@stockton.edu>
2018-10-09 10:49:22 -0400

    Buy a product added, styling on form, index, and buy views.

--- MVCDemoF18/Controllers/ProductsController.cs
+++ MVCDemoF18/Controllers/ProductsController.cs
@@ -9,21 +9,45 @@ namespace MVCDemoF18.Controllers
 {
     public class ProductsController : Controller
     {
-        private List<Product> products = new List<Product>();
+        private static List<Product> products = new List<Product>();
 
         public IActionResult Index()
         {
-            Product p = new Product
+            if (products.Count == 0)
             {
-                ProductID = 100,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Price = 750M,
-                Quantity = 2,
-                Category = "Watersports"
-            };
-            products.Add(p);
+                Product p = new Product
+                {
+                    ProductID = 100,
+                    Name = "Kayak",
+                    Description = "A boat for one person",
+                    Price = 750M,
+                    Quantity = 2,
+                    Category = "Watersports"
+                };
+                products.Add(p);
+            }
             return View(products);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            // add p to products list 
+            p.ProductID = products[products.Count - 1].ProductID + 1;
+            products.Add(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Buy(int id, int quantity)
+        {
+            var prod = products.Find(p => p.ProductID == id);
+            prod.Quantity = quantity;
+            return View(prod);
         }
     }
 }
